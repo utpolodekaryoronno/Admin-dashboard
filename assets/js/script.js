aa6cd1a2737a733b4b427ca8406418bc29a92972
Utpol Odekary Oronno <info.utpolodekary@gmail.com>
2024-06-12 12:57:04 +0600

    change
    change

--- assets/js/script.js
+++ assets/js/script.js
@@ -227,6 +227,119 @@ $(document).ready(function(){
 
 
 
+
+      // rich-alert close 
+      $(document).ready(function() {
+        $('.rich-alert-close').on('click', function(){
+            $('.rich-alert').hide();
+        }); 
+      });   
+
+      // toaster close 
+      $(document).ready(function() {
+        $('.toaster-close').on('click', function(){
+            $('.toaster1').hide();
+        }); 
+      });
+
+      $(document).ready(function() {
+        $('.toaster-close2').on('click', function(){
+            $('.toaster2').hide();
+        }); 
+      });
+
+
+
+
+      // Progressbar 
+
+      
+    $("#progress-bar1").animate({ 
+      width: "50%", 
+}, 3000); 
+
+$("#progress-bar2").animate({ 
+      width: "60%", 
+}, 3000); 
+
+$("#progress-bar3").animate({ 
+      width: "70%", 
+}, 3000); 
+
+$("#progress-bar4").animate({ 
+      width: "80%", 
+}, 3000); 
+
+$("#progress-bar5").animate({ 
+      width: "50%", 
+}, 3000); 
+
+$("#progress-bar6").animate({ 
+      width: "60%", 
+}, 3000); 
+
+$("#progress-bar7").animate({ 
+      width: "70%", 
+}, 3000); 
+
+$("#progress-bar8").animate({ 
+      width: "80%", 
+}, 3000); 
+
+$("#progress-bar9").animate({ 
+      width: "50%", 
+}, 3000); 
+
+$("#progress-bar10").animate({ 
+      width: "60%", 
+}, 3000); 
+
+$("#progress-bar11").animate({ 
+      width: "70%", 
+}, 3000); 
+
+$("#progress-bar12").animate({ 
+      width: "80%", 
+}, 3000); 
+
+$("#progress-bar13").animate({ 
+      width: "50%", 
+}, 3000); 
+
+$("#progress-bar14").animate({ 
+      width: "60%", 
+}, 3000); 
+
+$("#progress-bar15").animate({ 
+      width: "70%", 
+}, 3000); 
+
+$("#progress-bar16").animate({ 
+      width: "80%", 
+}, 3000);
+
+$("#progress-bar17").animate({ 
+      width: "50%", 
+}, 3000); 
+
+$("#progress-bar18").animate({ 
+      width: "60%", 
+}, 3000); 
+
+$("#progress-bar19").animate({ 
+      width: "70%", 
+}, 3000); 
+
+$("#progress-bar20").animate({ 
+      width: "80%", 
+}, 3000); 
+$("#progress-bar21").animate({ 
+      width: "90%", 
+}, 3000); 
+
+
+
+
   // Primary Loadning
   $(function(){
     var PrimaryLoading = document.querySelector('.PrimaryLoading');
@@ -326,6 +439,30 @@ $(function(){
 
 
 
+    // tooltip js 
+
+    $(function () {
+
+      $('#placement-top').tooltip({
+          'placement': 'top',
+      });
+
+      $('#placement-bottom').tooltip({
+          'placement': 'bottom',
+      });
+
+      $('#placement-right').tooltip({
+          'placement': 'right',
+      });
+
+      $('#placement-left').tooltip({
+          'placement': 'left',
+      });
+
+  })
+
+
+
 
 //  Tooltip
 var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
@@ -365,6 +502,12 @@ accordion();
 
 
 
+// daterangepicker
+$(function(){
+  $('#basic').daterangepicker();
+});
+
+
 
 
 // Datepicker 
@@ -373,6 +516,86 @@ $(document).ready(function(){
       autoclose: true,
       todayHighlight: true,
   });
+});
+
+
+// fullcalender 
+
+document.addEventListener('DOMContentLoaded', function() {
+  var calendarEl = document.getElementById('calendar');
+
+  var calendar = new FullCalendar.Calendar(calendarEl, {
+      plugins: [ 'interaction', 'dayGrid', 'timeGrid', 'list' ],
+      height: 'parent',
+      header: {
+      left: 'prev,next today',
+      center: 'title',
+      right: 'dayGridMonth,timeGridWeek,timeGridDay,listWeek'
+      },
+  defaultView: 'dayGridMonth',
+  defaultDate: '2023-02-12',
+  navLinks: true, // can click day/week names to navigate views
+  editable: true,
+  eventLimit: true, // allow "more" link when too many events
+  events: [
+    {
+      title: 'All Day Event',
+      start: '2023-02-01',
+    },
+    {
+      title: 'Long Event',
+      start: '2023-02-07',
+      end: '2023-02-10'
+    },
+    {
+      groupId: 999,
+      title: 'Repeating Event',
+      start: '2023-02-09T16:00:00'
+    },
+    {
+      groupId: 999,
+      title: 'Repeating Event',
+      start: '2023-02-16T16:00:00'
+    },
+    {
+      title: 'Conference',
+      start: '2023-02-11',
+      end: '2023-02-13'
+    },
+    {
+      title: 'Meeting',
+      start: '2023-02-12T10:30:00',
+      end: '2023-02-12T12:30:00'
+    },
+    {
+      title: 'Lunch',
+      start: '2023-02-12T12:00:00'
+    },
+    {
+      title: 'Meeting',
+      start: '2023-02-12T14:30:00'
+    },
+    {
+      title: 'Happy Hour',
+      start: '2023-02-12T17:30:00'
+    },
+    {
+      title: 'Dinner',
+      start: '2023-02-12T20:00:00'
+    },
+    {
+      title: 'Birthday Party',
+      start: '2023-02-13T07:00:00'
+    },
+    {
+      title: 'Click for Google',
+      url: 'http://google.com/',
+      start: '2023-02-28'
+    }
+  ]
+});
+
+calendar.render();
 });
 
  
